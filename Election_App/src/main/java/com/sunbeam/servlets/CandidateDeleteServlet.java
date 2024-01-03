9312ed6cd724c7c24ee1cf5bbe7b201d0429cfdf
Anurag Maldhure <hello.anurag.maldhure@gmail.com>
2024-01-03 18:02:15 +0530

    Delete candidate using query string

---
+++ Election_App/src/main/java/com/sunbeam/servlets/CandidateDeleteServlet.java
@@ -0,0 +1,36 @@
+package com.sunbeam.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import com.sunbeam.daos.ICandidateDao;
+import com.sunbeam.daos.CandidateDao;
+
+@WebServlet("/canddel")
+public class CandidateDeleteServlet extends HttpServlet {
+	@Override
+	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		processRequest(req, resp);
+	}	
+	@Override
+	protected void doPost(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		processRequest(req, resp);
+	}	
+	
+	protected void processRequest(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		String candId = req.getParameter("id");
+		int id = Integer.parseInt(candId);
+		int cnt = 0;
+		try(ICandidateDao dao = new CandidateDao()) {
+			cnt = dao.deleteById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		//resp.sendRedirect("result");
+		RequestDispatcher rd = req.getRequestDispatcher("result");
+		rd.forward(req, resp);
+	}
+}
